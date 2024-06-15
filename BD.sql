c1f2b7422f037008c33c0797e022808be1694c7d
LucasDBorja <143081328+LucasDBorja@users.noreply.github.com>
2024-06-15 15:23:39 -0300

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -1,12 +1,8 @@
 --Crie um banco de dados chamado "chat gost" caso não queira criar use essa tabela
-
--- Comando para criar o banco de dados, se não existir
 CREATE DATABASE IF NOT EXISTS `chat gost`;
 
--- Comando para usar o banco de dados criado
 USE `chat gost`;
 
--- Comando para criar a tabela `messages`, se não existir
 CREATE TABLE IF NOT EXISTS messages (
     id INT(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -15,7 +11,6 @@ CREATE TABLE IF NOT EXISTS messages (
     PRIMARY KEY (id)
 );
 
--- Comando para criar a tabela `users`, se não existir
 CREATE TABLE IF NOT EXISTS users (
     id INT(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
